31b31033232a59f39696ee43b8aad7a416b8b5b8
mgruben <GrubenM@gmail.com>
2017-01-16 15:03:10 -0600

    Throw an exception if any argument is null

--- Outcast.java
+++ Outcast.java
@@ -20,17 +20,25 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class Outcast {
-   public Outcast(WordNet wordnet)         // constructor takes a WordNet object
-   public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
-   public static void main(String[] args) {
-    WordNet wordnet = new WordNet(args[0], args[1]);
-    Outcast outcast = new Outcast(wordnet);
-    for (int t = 2; t < args.length; t++) {
-        In in = new In(args[t]);
-        String[] nouns = in.readAllStrings();
-        StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+        if (wordnet == null) throw new java.lang.NullPointerException();
     }
-}
+    
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+       if (nouns == null) throw new java.lang.NullPointerException();
+    }
+    
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
+    }
 
 }
 
--- SAP.java
+++ SAP.java
@@ -21,33 +21,43 @@
  */
 public class SAP {
 
-   // constructor takes a digraph (not necessarily a DAG)
-   public SAP(Digraph G)
-
-   // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w)
-
-   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-   public int ancestor(int v, int w)
-
-   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-   public int length(Iterable<Integer> v, Iterable<Integer> w)
-
-   // a common ancestor that participates in shortest ancestral path; -1 if no such path
-   public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
-
-   // do unit testing of this class
-   public static void main(String[] args) {
-    In in = new In(args[0]);
-    Digraph G = new Digraph(in);
-    SAP sap = new SAP(G);
-    while (!StdIn.isEmpty()) {
-        int v = StdIn.readInt();
-        int w = StdIn.readInt();
-        int length   = sap.length(v, w);
-        int ancestor = sap.ancestor(v, w);
-        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        if (G == null) throw new java.lang.NullPointerException();
     }
-   }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null | w == null) throw new java.lang.NullPointerException();
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null | w == null) throw new java.lang.NullPointerException();
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
 }
 
