4e6b9427d729f24faaade7e87221016050648418
mgruben <GrubenM@gmail.com>
2017-01-16 14:48:25 -0600

    Initialize API with signatures

---
+++ SAP.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class SAP {
+
+   // constructor takes a digraph (not necessarily a DAG)
+   public SAP(Digraph G)
+
+   // length of shortest ancestral path between v and w; -1 if no such path
+   public int length(int v, int w)
+
+   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+   public int ancestor(int v, int w)
+
+   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+   public int length(Iterable<Integer> v, Iterable<Integer> w)
+
+   // a common ancestor that participates in shortest ancestral path; -1 if no such path
+   public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
+
+   // do unit testing of this class
+   public static void main(String[] args)
+}
+
