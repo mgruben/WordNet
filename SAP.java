e146af564205f3bc30fad9c34ab03bb8aa012d63
mgruben <GrubenM@gmail.com>
2017-01-16 14:51:14 -0600

    Add test client

--- SAP.java
+++ SAP.java
@@ -37,6 +37,17 @@ public class SAP {
    public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
 
    // do unit testing of this class
-   public static void main(String[] args)
+   public static void main(String[] args) {
+    In in = new In(args[0]);
+    Digraph G = new Digraph(in);
+    SAP sap = new SAP(G);
+    while (!StdIn.isEmpty()) {
+        int v = StdIn.readInt();
+        int w = StdIn.readInt();
+        int length   = sap.length(v, w);
+        int ancestor = sap.ancestor(v, w);
+        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+    }
+   }
 }
 
