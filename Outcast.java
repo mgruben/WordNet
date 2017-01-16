e3b42f54f6446b6061e0ff1947ebdf2f31336eaa
mgruben <GrubenM@gmail.com>
2017-01-16 14:49:02 -0600

    Initialize API with signatures

---
+++ Outcast.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class Outcast {
+   public Outcast(WordNet wordnet)         // constructor takes a WordNet object
+   public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
+   public static void main(String[] args)  // see test client below
+}
+
