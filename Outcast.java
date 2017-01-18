846f47355dbc6f855e0ba7261d042a4a9a949de9
mgruben <GrubenM@gmail.com>
2017-01-18 00:14:40 -0600

    Implement wordnet comparison

--- Outcast.java
+++ Outcast.java
@@ -24,6 +24,8 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Michael <GrubenM@GMail.com>
  */
 public class Outcast {
+    WordNet wordnet;
+    
     /**
      * Constructor takes a WordNet object.
      * 
@@ -32,6 +34,7 @@ public class Outcast {
      */
     public Outcast(WordNet wordnet) {
         if (wordnet == null) throw new java.lang.NullPointerException();
+        this.wordnet = wordnet;
     }
     
     /**
@@ -43,6 +46,19 @@ public class Outcast {
      */
     public String outcast(String[] nouns) {
        if (nouns == null) throw new java.lang.NullPointerException();
+       int sum = 0;
+       String out = "";
+       for (int i = 0; i < nouns.length; i++) {
+           int d = 0;
+           for (int j = 0; j < nouns.length; j++) {
+               d += wordnet.distance(nouns[i], nouns[j]);
+           }
+           if (d > sum) {
+               sum = d;
+               out = nouns[i];
+           }
+       }
+       return out;
     }
     
     public static void main(String[] args) {
